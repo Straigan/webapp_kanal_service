f37312c50df9da68e250aadc1fda9c63297186e3
Ilya Styapin <istyapin@gmail.com>
2023-03-21 00:12:11 +0500

    Create service get and insert data in DB

--- webapp/kanal_service/views.py
+++ webapp/kanal_service/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from webapp.services.google_sheet import insert_data_in_db
 
 
 blueprint = Blueprint('kanal_service', __name__)
@@ -6,4 +7,5 @@ blueprint = Blueprint('kanal_service', __name__)
 @blueprint.route('/')
 def index():
     page_title = "Каналcервис"
+    insert_data_in_db()
     return render_template('kanal_service/index.html', page_title=page_title)
