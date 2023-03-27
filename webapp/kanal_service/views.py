146b0342f6d7a824d8fd0298ce5a9cc2ccec424b
Ilya Styapin <istyapin@gmail.com>
2023-03-27 23:46:01 +0500

    Add view table orde in index page

--- webapp/kanal_service/views.py
+++ webapp/kanal_service/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+
+from webapp.kanal_service.models import Order
 from webapp.services.google_sheet import insert_data_in_db
 
 
@@ -7,5 +9,5 @@ blueprint = Blueprint('kanal_service', __name__)
 @blueprint.route('/')
 def index():
     page_title = "Каналcервис"
-    insert_data_in_db()
-    return render_template('kanal_service/index.html', page_title=page_title)
+    orders = Order.query.all()
+    return render_template('kanal_service/index.html', page_title=page_title, orders=orders)
