206d3234a189d17acf59a5cd7a57e3865aa6a293
Ilya Styapin <istyapin@gmail.com>
2023-04-11 23:48:26 +0500

    Add grafic orders on index.html

--- webapp/kanal_service/views.py
+++ webapp/kanal_service/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from pandas import to_datetime
 
 from webapp.kanal_service.models import Order
 
@@ -9,4 +10,6 @@ blueprint = Blueprint('kanal_service', __name__)
 def index():
     page_title = "Каналcервис"
     orders = Order.query.all()
-    return render_template('kanal_service/index.html', page_title=page_title, orders=orders)
+    labels = [order.date_of_delivery for order in orders]
+    data = [order.price_in_dollar for order in orders]
+    return render_template('kanal_service/index.html', page_title=page_title, orders=orders, labels=labels, data=data)
