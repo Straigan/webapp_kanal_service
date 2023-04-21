ba9fa5d6ae046a06580fe16b289b172cd4584be6
Ilya Styapin <istyapin@gmail.com>
2023-04-21 05:13:10 +0500

    add CI/CD v1

--- webapp/kanal_service/views.py
+++ webapp/kanal_service/views.py
@@ -12,4 +12,11 @@ def index():
     orders = Order.query.all()
     labels = [order.date_of_delivery for order in orders]
     data = [order.price_in_dollar for order in orders]
-    return render_template('kanal_service/index.html', page_title=page_title, orders=orders, labels=labels, data=data)
+    total_sum_dollar = sum(data)
+    return render_template('kanal_service/index.html', 
+                    page_title=page_title, 
+                    orders=orders, 
+                    labels=labels, 
+                    data=data, 
+                    total_sum_dollar=total_sum_dollar
+                )
